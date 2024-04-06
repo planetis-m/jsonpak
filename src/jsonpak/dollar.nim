0be8b15d4e16f717253c0ca7b4ac817e7679768d
Antonis Geralis <b3lievem@yandex.com>
2024-04-06 14:23:16 +0300

    remove

--- src/jsonpak/dollar.nim
+++ src/jsonpak/dollar.nim
@@ -1,29 +1,5 @@
-from std/strutils import toHex
+from std/json import escapeJsonUnquoted, escapeJson
 import private/[bitabs, jsonnode, jsontree], std/importutils, ssostrings
-
-proc escapeJsonUnquoted(s: String; result: var string) =
-  ## Converts a string `s` to its JSON representation without quotes.
-  ## Appends to `result`.
-  for c in s:
-    case c
-    of '\L': result.add("\\n")
-    of '\b': result.add("\\b")
-    of '\f': result.add("\\f")
-    of '\t': result.add("\\t")
-    of '\v': result.add("\\u000b")
-    of '\r': result.add("\\r")
-    of '"': result.add("\\\"")
-    of '\0'..'\7': result.add("\\u000" & $ord(c))
-    of '\14'..'\31': result.add("\\u00" & toHex(ord(c), 2))
-    of '\\': result.add("\\\\")
-    else: result.add(c)
-
-proc escapeJson(s: String; result: var string) =
-  ## Converts a string `s` to its JSON representation with quotes.
-  ## Appends to `result`.
-  result.add("\"")
-  escapeJsonUnquoted(s, result)
-  result.add("\"")
 
 proc add(dest: var string; src: String) {.inline.} =
   setLen(dest, dest.len+src.len)
@@ -99,7 +75,7 @@ proc toUgly*(result: var string, tree: JsonTree, n: NodePos) =
           result.add ","
           pendingComma = false
         if action == actionKeyVal:
-          key.escapeJson(result)
+          toNimStr(key).escapeJson(result)
           result.add ":"
         case child.kind
         of opcodeArray:
@@ -114,7 +90,7 @@ proc toUgly*(result: var string, tree: JsonTree, n: NodePos) =
           result.add child.str
           pendingComma = true
         of opcodeString:
-          escapeJson(child.str, result)
+          escapeJson(toNimStr(child.str), result)
           pendingComma = true
         of opcodeBool:
           result.add(if child.bval: "true" else: "false")
@@ -128,7 +104,7 @@ proc toUgly*(result: var string, tree: JsonTree, n: NodePos) =
     else:
       result.add "}"
   of opcodeString:
-    escapeJson(n.str, result)
+    escapeJson(toNimStr(n.str), result)
   of opcodeInt, opcodeFloat:
     result.add n.str
   of opcodeBool:
